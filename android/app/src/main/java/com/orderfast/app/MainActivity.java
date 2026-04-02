55ce698f991611ee09a657182450c76803a7dda9
Fodel2 <142302099+Fodel2@users.noreply.github.com>
2026-04-02 18:50:42 +0100

    fix(kiosk-apk): refresh canonical release asset and trace build

--- android/app/src/main/java/com/orderfast/app/MainActivity.java
+++ android/app/src/main/java/com/orderfast/app/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.WindowManager;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.webkit.WebBackForwardList;
+import android.widget.Toast;
 
 import com.getcapacitor.BridgeActivity;
 import android.webkit.WebView;
@@ -23,6 +24,7 @@ public class MainActivity extends BridgeActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         applyImmersiveMode();
         configureWebViewPresentation();
+        Toast.makeText(this, "Build " + BuildConfig.VERSION_NAME, Toast.LENGTH_LONG).show();
     }
 
     @Override
